cc607ccd99eff54a39619d61a314f85954757d9d
Flavio Garcia <piraz@candango.org>
2019-06-24 01:28:24 -0400

    Added basic vigiles(agent) loop to the project.
    The loop will be moved to the vigiles component in the future we
    need to convert this project to multi project.
    
    Refs: #2

--- mncitadel/app.py
+++ mncitadel/app.py
@@ -1,12 +1,35 @@
 from mncitadel import handlers
 from firenado import tornadoweb
+import logging
+import tornado.ioloop
+
+
+logger = logging.getLogger(__name__)
 
 
 class MNCitadelComponent(tornadoweb.TornadoComponent):
+
+    def __init__(self, name, application):
+        tornadoweb.TornadoComponent.__init__(self, name, application)
+        self.vigiles_loop = None
 
     def get_handlers(self):
         return [
             (r'/', handlers.IndexHandler),
         ]
 
+    def initialize(self):
+        logger.info("Starting vigiles.")
+        loop_interval = 1000
+        logger.info("Configuring vililes timmer as %sms" % loop_interval)
+        self.vigiles_loop = tornado.ioloop.PeriodicCallback(
+            self.watch_over,
+            loop_interval
+        )
+        self.vigiles_loop.start()
 
+    def watch_over(self):
+        self.vigiles_loop.stop()
+        logger.info("Watching over...")
+        logger.info("Done.")
+        self.vigiles_loop.start()
--- mncitadel/handlers.py
+++ mncitadel/handlers.py
@@ -4,4 +4,4 @@ from firenado import tornadoweb
 class IndexHandler(tornadoweb.TornadoHandler):
 
     def get(self):
-        self.write("IndexHandler output")
+        self.write("Rocca output")
